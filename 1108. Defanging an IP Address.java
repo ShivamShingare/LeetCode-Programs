323a79edca09da8055b586f2ab0b60b579372434
Shivam Shingare <shivam.s2818@gmail.com>
2025-01-14 18:46:08 +0530

    Add files via upload

---
+++ 1108. Defanging an IP Address.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public String defangIPaddr(String address) 
+    {
+     
+    int n = address.length();
+    String ans = "";
+        
+    for(int i=0;i<n;i++)
+    {
+      if(address.charAt(i) == '.')
+      {
+        ans += "[.]";
+      }
+      else
+      {
+         ans += address.charAt(i);
+      }
+    }
+        return ans;
+    }
+}
