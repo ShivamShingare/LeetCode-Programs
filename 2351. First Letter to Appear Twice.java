716af52205d992711aaf02e42ea3e9ea0b8aedec
Shivam Shingare <shivam.s2818@gmail.com>
2025-02-15 19:00:57 +0530

    Add files via upload

---
+++ 2351. First Letter to Appear Twice.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public char repeatedCharacter(String s) 
+    {
+        
+        HashMap<Character,Integer> hm = new HashMap<>();
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+
+            if(hm.get(ch) == 2)
+            {
+                return ch;
+            }
+        }
+
+        
+        return '\0';
+    }
+}
